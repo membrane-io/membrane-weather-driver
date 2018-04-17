8bfdf21ea711e56afba5795d8a3433cd4147d59a
aranajhonny <jhonnyjosearana@gmail.com>
2018-04-16 22:58:23 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,4 @@
+const { schema, imports, dependencies, environment, expressions, endpoints } = program;
+
+schema.type('Root')
+ 
---
+++ index.js
@@ -0,0 +1,14 @@
+const { root } = program.refs
+
+export async function init() {
+  // Called when the program is run
+}
+
+export async function update() {
+  // Called when the program is updated from a previous version
+}
+
+export async function timer({ key }) {
+  // Called every time a timer fires
+}
+
