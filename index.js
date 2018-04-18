d6da5b3c9fdc5d5eb994c9b1a1bcd2e71653a56e
aranajhonny <jhonnyjosearana@gmail.com>
2018-04-18 12:54:41 -0400

    ...

--- index.js
+++ index.js
@@ -19,10 +19,23 @@ export const LocationCollection = {
 	},
 };
 
+export const ListCollection = {
+	one({ source, args }) {
+		const item = source.find(one => one.dt === args.dt);
+		return item;
+	},
+	items({ source }) {
+		return source;
+	},
+};
+
 export const ListItem = {
-	async self({ source, self, parent }) {
-		const { zip, country } = self.match(root.locations.byZipCode());
-		return root.locations.byZipCode({ zip: zip, country: country });
+	async self({ source, self , parent}) {
+		// or self.match ?
+		const { zip, country } = parent.match(root.locations.byZipCode());
+		return root.locations
+			.byZipCode({ country: 'us', zip: '94040' }) // fake data
+			.list.one({ dt: source.dt });
 	},
 };
 
