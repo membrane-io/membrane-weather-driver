5f15435782e0f9b50e4cf1d88a8af811ee685940
aranajhonny <jhonnyjosearana@gmail.com>
2018-04-19 22:46:11 -0400

    some changes

--- index.js
+++ index.js
@@ -4,48 +4,48 @@ const { APPID } = process.env;
 import { get } from './client';
 
 export async function init() {
-	await root.set({
-		locations: {},
-	});
+  await root.set({
+    locations: {},
+  });
 }
 
 export const LocationCollection = {
-	async byZipCode({ args }) {
-		const { country, zip } = args;
-		const result = await get(
-			`/forecast?zip=${zip},${country}&APPID=${APPID}`
-		);
-		return result;
-	},
+  async byZipCode({ args }) {
+    const { country, zip } = args;
+    const result = await get(`/forecast?zip=${zip},${country}&APPID=${APPID}`);
+    return result;
+  },
 };
 
-export const ListCollection = {
-	one({ source, args }) {
-		const item = source.find(one => one.dt === args.dt);
-		return item;
-	},
-	items({ source }) {
-		return source;
-	},
+export const Location = {
+  forecasts({ source }) {
+    return source['list'];
+  },
+};
+
+export const ForecastCollection = {
+  one({ source, args }) {
+    const hourEpoch = args.hours * 3600 - new Date().getTime();
+    const item = source.find(one => one.dt * 1000 > hourEpoch);
+    return item;
+  },
+  items({ source }) {
+    return source;
+  },
 };
 
 export const ListItem = {
-	async self({ source, self }) {
-		const { zip, country } = self.match(root.locations.byZipCode());
-		return root.locations
-			.byZipCode({ country: us, zip: zip })
-			.list.one({ dt: source.dt });
-	},
-};
-
-export const RainInfo = {
-	last3h({ source }) {
-		return source['3h'];
-	},
-};
-
-export const SnowInfo = {
-	last3h({ source }) {
-		return source['3h'];
-	},
+  async self({ source, self, parent }) {
+    const { zip, country } = parent.match(root.locations.byZipCode());
+    return root.locations
+      .byZipCode({ country: country, zip: zip })
+      .forecasts()
+      .one({ dt: source.dt });
+  },
+  weather({ source }) {
+    return source['weather'][0];
+  },
+  dtTxt({ source }) {
+    return source['dt_txt'];
+  },
 };
