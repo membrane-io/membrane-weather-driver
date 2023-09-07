2753af25b5f8e0d38d1fe8bc2238e81487dbf077
Juan Campa <juancampa@gmail.com>
2023-09-07 16:21:57 -0400

    Simplify impl using Json fields

--- index.ts
+++ index.ts
@@ -1,33 +1,39 @@
 // `nodes` contain any nodes you add from the graph (dependencies)
 // `root` is a reference to this program's root node
 // `state` is an object that persists across program updates. Store data here.
-import { nodes, root, state } from "membrane";
+import { state } from "membrane";
 import { api } from "./utils";
+
+state.latLon = state.latLon ?? {};
 
 export const Root = {
   status: () => {
     if (!state.apiKey) {
-      return "Invoke `:configure` with your openweathermap.org API key first";
+      return "Get an [OpenWeather API key](https://openweathermap.org) and then [configure](:configure) it.";
     }
-    return `Weather for ${state.cityData.name}`;
+    return `Ready`;
   },
-  async configure({ args: { apiKey, zipCode, countryCode } }) {
-    state.apiKey = apiKey;
-    // Retrieve lat/lon for the given zip/country code
-    const query = {
-      zip: `${zipCode},${countryCode}`,
-    };
-    const res = await api("GET", "geo/1.0/zip", query);
-    // Save the retrieved city data in the state object
-    state.cityData = await res.json();
+  async configure({ args: { apiKey } }) {
+    state.apiKey = apiKey ?? state.apiKey;
   },
-  async weather({ args: { units }}) {
+  async weather({ args: { units, zipCode, countryCode } }) {
+    const zip = `${zipCode},${countryCode ?? "US"}`;
+
+    // Retrieve and cache lat/lon for the given zip/country code
+    if (!state.latLon[zip]) {
+      const res = await api("geo/1.0/zip", { zip });
+      const { lat, lon } = await res.json();
+      state.latLon[zip] = { lat, lon };
+    }
+
     const query = {
-      lat: state.cityData.lat,
-      lon: state.cityData.lon,
-      units: units || "imperial",
+      ...state.latLon[zip],
+      units: units ?? "standard",
     };
-    const res = await api("GET", "data/2.5/onecall", query);
+    const res = await api("data/2.5/onecall", query);
+    if (res.status !== 200) {
+      throw new Error(`OpenWeather API returned ${res.status}: ${res.text()}`);
+    }
     return await res.json();
   },
 };
@@ -38,32 +44,3 @@ export const Weather = {
   daily: ({ obj }) => obj.daily,
   minutely: ({ obj }) => obj.minutely,
 };
-
-export const MinutelyCollection = {
-  one: ({ obj, args: { dt } }) => findOneByDt(obj, dt),
-  items: ({ obj }) => obj,
-};
-
-export const HourlyCollection = {
-  one: ({ obj, args: { dt } }) => findOneByDt(obj, dt),
-  items: ({ obj }) => obj,
-};
-
-export const DailyCollection = {
-  one: ({ obj, args: { dt } }) => findOneByDt(obj, dt),
-  items: ({ obj }) => obj,
-};
-
-export const Minutely = {
-  gref: ({ obj }) => root.weather.minutely.one({ dt: obj.dt }),
-};
-
-export const Hourly = {
-  gref: ({ obj }) => root.weather.hourly.one({ dt: obj.dt }),
-};
-
-export const Daily = {
-  gref: ({ obj }) => root.weather.daily.one({ dt: obj.dt }),
-};
-
-const findOneByDt = (obj, dt) => obj.find((one) => one.dt === dt);
